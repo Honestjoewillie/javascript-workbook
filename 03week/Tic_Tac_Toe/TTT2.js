6e86613fcd783c9eef4429dbed14bab731c83d37
Joseph William Hauger <joseph>
2019-04-20 13:04:33 -0500

    final draft of TTTGui

--- 03week/Tic_Tac_Toe/TTT2.js
+++ 03week/Tic_Tac_Toe/TTT2.js
@@ -1,6 +1,8 @@
 'use strict';
 
-
+//this will set the board at the beginning
+//it will also reset the game a new
+//as well as randomly pick who should start the game
 function startGame() {
     
     for (let i = 1; i <= 9; i++){
@@ -14,11 +16,12 @@ function startGame() {
     document.winner = null;
     setMessage(document.turn + " gets to start!");
 }
-
+//tells people who's move, who won and what is going on.
 function setMessage(msg){
     document.getElementById("message").innerText = msg;
 }
-
+//this stops someone from placing their turn on top of an already occupied square
+//it will also send a notification saying: can't do that!
 function nextMove(square){
     if (document.winner !== null) {
         setMessage(document.winner + " already won the game.");
@@ -32,6 +35,7 @@ function nextMove(square){
     }
 }
 
+//first see if there is a winner, if not next person gets a turn
 function switchTurn() {
 
     if (checkForWinner(document.turn)){
@@ -47,7 +51,7 @@ function switchTurn() {
         setMessage("It's " + document.turn + "'s turn!");
     }
 }
-
+//check to see if someone has won the game
 function checkForWinner(move){
     let result= false;
     if (checkRow(1, 2, 3, move) ||
@@ -64,6 +68,7 @@ function checkForWinner(move){
     return result;
 }
 
+//this is how we check on is there a winner?
 function checkRow(a, b, c, move) {
     let result = false;
     if (getBox(a) === move && getBox(b) === move && getBox(c) === move) {
@@ -71,11 +76,11 @@ function checkRow(a, b, c, move) {
     }
     return result;
 }
-
+//used in the checkRow function to see if there is a X or O in a box
 function getBox(number){
     return document.getElementById("s" + number).innerText;
 }
-
+//this is used to restart the game...if there is an X or O in the box it removes it and makes the box empty
 function clearBox(number) {
     document.getElementById("s" + number).innerText = "";
 }
