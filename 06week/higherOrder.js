8bb161377eb385366ea895c192fa3b495ccbfc52
Joseph William Hauger <joseph>
2019-05-07 16:35:04 -0500

    still not working

--- 06week/higherOrder.js
+++ 06week/higherOrder.js
@@ -14,7 +14,6 @@ function map(arr, callback) {
     newArray.push(elements);
   }
   return newArray;
-  console.log(map);
 }
 const mapIt = map(arr, function(n){
   n =n*2;
@@ -22,45 +21,74 @@ const mapIt = map(arr, function(n){
 })
 console.log(mapIt);
 
-const food = [
+const object = [
   {apple: 50},
   {pizza: 10},
   {pie: 20}
 ];
-let newFoodArray = [];
-for(let index = 0; index < food.length; index++){
-  for(let i in food[index]){
-    newFoodArray.push(food[index][i]);
+let newObject = [];
+for(let index = 0; index < object.length; index++){
+  for(let i in object[index]){
+    newObject.push(object[index][i]);
   }
 }
-const sum1 = newFoodArray.reduce(function(accumulator, currentValue){
-  return accumulator + currentValue;
-}, 0)
-const yep = sum1.toString().split();
-console.log(yep);
-
-const numbers = [50, 10, 20].reduce(function(result, item){
-  return result + item;
-}, 0);
-const final = numbers.toString().split();
-console.log(final);
-
-const foodTwo = [
-  {apple: 40},
-  {pizza: 17},
-  {pie: 2}
-];
-let newFoodArrayTwo = [];
-for(let index = 0; index < foodTwo.length; index++){
-  for(let i in foodTwo[index]){
-    newFoodArrayTwo.push(foodTwo[index][i]);
-  }
+function reduce(array, accumulator) {
+    accumulator = accumulator || 0;
+    for (let index = 0; index < array.length; index++) {
+        if(typeof array[index] == 'number') {
+            accumulator = accumulator + array[index];
+        } else if(typeof array[index] == 'object') {
+            for(let i in array[index]){
+                accumulator = accumulator + array[index][i];
+            }
+        }   
+    }
+    return accumulator;
 }
-const sum2 = newFoodArrayTwo.filter(function(num){
-  return num % 2 === 0;
-});
+const sum = reduce(object, function(a, b){
+  return a+b;
+})
+console.log(sum);
 
-console.log(sum2);
+// const food = [
+//   {apple: 50},
+//   {pizza: 10},
+//   {pie: 20}
+// ];
+// let newFoodArray = [];
+// for(let index = 0; index < food.length; index++){
+//   for(let i in food[index]){
+//     newFoodArray.push(food[index][i]);
+//   }
+// }
+// const sum1 = newFoodArray.reduce(function(accumulator, currentValue){
+//   return accumulator + currentValue;
+// }, 0)
+// const yep = sum1.toString().split();
+// console.log(yep);
+
+// const numbers = [50, 10, 20].reduce(function(result, item){
+//   return result + item;
+// }, 0);
+// const final = numbers.toString().split();
+// console.log(final);
+
+// const foodTwo = [
+//   {apple: 40},
+//   {pizza: 17},
+//   {pie: 2}
+// ];
+// let newFoodArrayTwo = [];
+// for(let index = 0; index < foodTwo.length; index++){
+//   for(let i in foodTwo[index]){
+//     newFoodArrayTwo.push(foodTwo[index][i]);
+//   }
+// }
+// const sum2 = newFoodArrayTwo.filter(function(num){
+//   return num % 2 === 0;
+// });
+
+// console.log(sum2);
 
 
 if (typeof describe === 'function') {
