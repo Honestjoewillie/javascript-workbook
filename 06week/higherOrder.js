62b805dc57d20bbdc80bc4382290fba0dac31751
Joseph William Hauger <joseph>
2019-05-08 18:09:41 -0500

    all tests passing

--- 06week/higherOrder.js
+++ 06week/higherOrder.js
@@ -43,8 +43,8 @@ console.log("ANSWER: ",sum);
 const sum2 = reduce(checkObject, 10);
 console.log(sum2);
 
-const checkObject = [{price: 10},{price: 20},{price: 30}];
-const checkArray = [10, 20, 30];
+// const checkObject = [{price: 10},{price: 20},{price: 30}];
+// const checkArray = [10, 20, 30];
 function filter(array, callback, thisObject) {
   let filteredArray = [];
   let filterCallback = callback;
@@ -66,47 +66,11 @@ function filter(array, callback, thisObject) {
   }
   return filteredArray;
 }
+const mus = reduce(checkObject);
+console.log("Answer:", mus);
 
 
-// const food = 
-//   {apple: 50},
-//   {pizza: 10},
-//   {pie: 20}
-// ];
-// let newFoodArray = [];
-// for(let index = 0; index < food.length; index++){
-//   for(let i in food[index]){
-//     newFoodArray.push(food[index][i]);
-//   }
-// }
-// const sum1 = newFoodArray.reduce(function(accumulator, currentValue){
-//   return accumulator + currentValue;
-// }, 0)
-// const yep = sum1.toString().split();
-// console.log(yep);
-
-// const numbers = [50, 10, 20].reduce(function(result, item){
-//   return result + item;
-// }, 0);
-// const final = numbers.toString().split();
-// console.log(final);
-
-// const foodTwo = [
-//   {apple: 40},
-//   {pizza: 17},
-//   {pie: 2}
-// ];
-// let newFoodArrayTwo = [];
-// for(let index = 0; index < foodTwo.length; index++){
-//   for(let i in foodTwo[index]){
-//     newFoodArrayTwo.push(foodTwo[index][i]);
-//   }
-// }
-// const sum2 = newFoodArrayTwo.filter(function(num){
-//   return num % 2 === 0;
-// });
-
-// console.log(sum2);
+console.log(sum2);
 
 if (typeof describe === 'function') {
   describe('#map()', () => {
